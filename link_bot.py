8fbaacee578eb16ac07788a30c891ac5558276d4
Hanzo15484 <pikach968@gmail.com>
2025-10-06 16:16:00 +0530

    Update link_bot.py

--- link_bot.py
+++ link_bot.py
@@ -67,15 +67,15 @@ LIST_CHANNELS_PAGE_SIZE = 10
 
 #Search Range
 SEARCH_CHANNEL = range(1)
-# small caps
-#def to_small_caps(text: str) -> str:
-    #small_caps_map = {
-       # "a": "ᴀ", "b": "ʙ", "c": "ᴄ", "d": "ᴅ", "e": "ᴇ", "f": "ғ", "g": "ɢ",
-       # "h": "ʜ", "i": "ɪ", "j": "ᴊ", "k": "ᴋ", "l": "ʟ", "m": "ᴍ", "n": "ɴ",
-       # "o": "ᴏ", "p": "ᴘ", "q": "ǫ", "r": "ʀ", "s": "s", "t": "ᴛ", "u": "ᴜ",
-        #"v": "ᴠ", "w": "ᴡ", "x": "x", "y": "ʏ", "z": "ᴢ"
- #   }
-   # return "".join(small_caps_map.get(ch.lower(), ch) for ch in text)
+ small caps
+def to_small_caps(text: str) -> str:
+    small_caps_map = {
+        "a": "ᴀ", "b": "ʙ", "c": "ᴄ", "d": "ᴅ", "e": "ᴇ", "f": "ғ", "g": "ɢ",
+        "h": "ʜ", "i": "ɪ", "j": "ᴊ", "k": "ᴋ", "l": "ʟ", "m": "ᴍ", "n": "ɴ",
+        "o": "ᴏ", "p": "ᴘ", "q": "ǫ", "r": "ʀ", "s": "s", "t": "ᴛ", "u": "ᴜ",
+        "v": "ᴠ", "w": "ᴡ", "x": "x", "y": "ʏ", "z": "ᴢ"
+    }
+    return "".join(small_caps_map.get(ch.lower(), ch) for ch in text)
 
 def start(update, context):
     try:
@@ -2759,6 +2759,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 
